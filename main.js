3d1cdef1d239c83b65a12c3930915303c5d41b2e
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-05-16 18:55:26 -0700

    add about me etc

--- main.js
+++ main.js
@@ -6,9 +6,6 @@ window.addEventListener("scroll", () => {
   const scroll = window.pageYOffset || document.documentElement.scrollTop;
   const offset = work + scroll;
   const windowHeight = window.innerHeight;
-  // console.log(
-  //   `work: ${work}, scroll: ${scroll}, offset: ${offset}, windowHeight: ${windowHeight}`
-  // );
 
   if (scroll > offset - 200) {
     workTarget.forEach((work) => {
@@ -20,8 +17,10 @@ window.addEventListener("scroll", () => {
 // Slider for About page
 let index = -1;
 const slideImg = () => {
+  document.getElementById("slideImg").src = "";
+  document.getElementById("caption-for-img").innerText = "";
   const imgSrc = [
-    "https://2.bp.blogspot.com/-VA8DS8XXYKw/WLEurNb7xVI/AAAAAAABCF4/8oFo3A3VlYkQWi6YV3AhaYh8ndqn_bPtgCLcB/s800/computer07_woman.png",
+    "./img/meCoding.JPG",
     "./img/dance.JPG",
     "./img/drawing.JPG",
     "./img/traveling.JPG",
@@ -39,7 +38,7 @@ const slideImg = () => {
     index++;
   }
   document.getElementById("slideImg").src = imgSrc[index];
-  document.getElementById("caption-for-img").innerHTML = captionForImg[index];
+  document.getElementById("caption-for-img").innerText = captionForImg[index];
   const fadedInEl = document.querySelectorAll(".faded-in");
   fadedInEl.forEach((val) => {
     val.classList.add("fade-in-animation");
@@ -55,6 +54,7 @@ const close = document.getElementById("close");
 const modal = document.getElementById("modal");
 const mask = document.getElementById("modalMask");
 const modalActivated = document.getElementById("modalActivated");
+let selectedWork;
 
 open.forEach((workEl) => {
   workEl.addEventListener("click", () => {
@@ -66,6 +66,7 @@ open.forEach((workEl) => {
     // get center position for modal
     modal.style.left = `${window.innerWidth / 2 - 400}px`;
     modal.style.top = `${window.pageYOffset + window.innerHeight / 2 - 200}px`;
+    selectedWork = workEl.getAttribute("data-value");
   });
 });
 
@@ -81,7 +82,7 @@ mask.addEventListener("click", function () {
   modalActivated.classList.remove("modal-activated");
 });
 
-// console.log(window.innerHeight);
-// console.log(window.innerWidth);
-// console.log(window.pageYOffset);
-// console.log(window.pageXOffset);
+// show each detail of work
+const showDetail = () => {
+  const viewApplicationBtn = document.getElementById("viewApplicationBtn");
+};
